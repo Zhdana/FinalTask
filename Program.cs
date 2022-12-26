1835c79e53c81aa3a92825e4d0ef3abc4a91fc5a
Zhdana <zhdanasulina@yandex.ru>
2022-12-26 12:38:19 +0300

    replaced the drawing and redact Readme

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[5] {":)", "2023", "Hello", "Hi!","GB"};
+string[] array2 = new string[array1.Length];
+
+void CompletionArray(string[] array1, string[] array2)
+{
+    int j=0;
+    for (int i=0; i<array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[j]=array1[i];
+            j++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+CompletionArray(array1, array2);
+PrintArray(array2);
